ae7c2d9acd2d531ec283206efffd54c334ba0bff
Philbert Yoon <yngpil.yoon@gmail.com>
2019-01-31 08:56:15 +0900

    Minor updates

--- zsh/.extra.zsh
+++ zsh/.extra.zsh
@@ -1,6 +1,11 @@
+#!/usr/bin/env zsh
+
 # Initialize pyenv
 command -v pyenv &> /dev/null && eval "$(pyenv init -)"
 command -v pyenv-virtualenv &> /dev/null && eval "$(pyenv virtualenv-init -)"
 
 # Initialize goenv
 command -v goenv &> /dev/null && eval "$(goenv init -)"
+
+# Setup direnv
+eval "$(direnv hook zsh)"
